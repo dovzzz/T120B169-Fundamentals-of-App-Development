cef9ffa4566a52f19134edf1ef7670063eca3589
dovzzz <dovilezinkute@gmail.com>
2022-03-07 15:42:33 +0200

    option to add task manually/ with qr code
    just options, no further functionality over these two options

--- StudIn/app/src/main/java/com/example/studin/MainActivity.java
+++ StudIn/app/src/main/java/com/example/studin/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.studin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
         Spinner dropdown = findViewById(R.id.calendarDropDown);
         String[] items = new String[]{"Month", "Week", "Day", "Calendar View"};
 
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //create an adapter to describe how the items are displayed,
+        //adapters are used in several places in android.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
         //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
 
@@ -35,11 +37,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // on click opens data input view for new event
-                Intent intent = new Intent(getBaseContext(), addNewEvent.class);
 
-                startActivity(intent);
+                showAlertDialogButtonClicked(view);
+
             }
         });
+    }
+
+    public void showAlertDialogButtonClicked(View view) {
+
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add new task");
+
+        // add a list
+        String[] options = {"Manually", "Scan QR code"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0: // manually
+                        Intent intent = new Intent(getBaseContext(), addNewEvent.class);
+                        startActivity(intent);
+                    case 1: // qr code
+                }
+            }
+        });
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
