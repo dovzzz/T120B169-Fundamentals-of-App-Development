5a5d064c89b1b702182f81dc9e2430d49eb2e093
Raviolliox <65919204+Raviolliox@users.noreply.github.com>
2022-03-08 04:42:33 +0200

    Added a taskbar with several options (settings, my events, about, create new event), added several views)

---
+++ StudIn/app/src/main/java/com/example/studin/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.studin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonAddEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.calendarDropDown);
+        String[] items = new String[]{"Month", "Week", "Day", "Calendar View"};
+
+        //create an adapter to describe how the items are displayed,
+        //adapters are used in several places in android.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        buttonAddEvent = (Button) findViewById(R.id.buttonAddEvent);
+        buttonAddEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // on click opens data input view for new event
+
+                showAlertDialogButtonClicked(view);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent newInt = new Intent(this, myEventActivity.class);
+                startActivity(newInt);
+                return true;
+            case R.id.item2:
+                Intent newInt2 = new Intent(this, settingsActivity.class);
+                startActivity(newInt2);
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "About was selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Already in event creation screen", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showAlertDialogButtonClicked(View view) {
+
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add new task");
+
+        // add a list
+        String[] options = {"Manually", "Scan QR code"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0: // manually
+                        Intent intent = new Intent(getBaseContext(), addNewEvent.class);
+                        startActivity(intent);
+                    case 1: // qr code
+                }
+            }
+        });
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
