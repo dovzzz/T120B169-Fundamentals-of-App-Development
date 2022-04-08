a8813a7b7f785464ee36c55deb5a045c4ca86003
dovzzz <dovilezinkute@gmail.com>
2022-04-08 12:59:14 +0300

    new project
    with menu from the left side. empty

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # T120B169-Fundamentals-of-App-Development
 Appsas moduliui "Mobilių programėlių kūrimo pagrindai". Tema: Semestro darbų planuotojas.
+
+# colors:
+res>values>colors.xml
+# string values:
+res>values>strings.xml
+
--- StudIn/app/src/main/java/com/example/studin/MainActivity.java
+++ StudIn/app/src/main/java/com/example/studin/MainActivity.java
@@ -1,109 +1,65 @@
 package com.example.studin;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.studin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonAddEvent;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.calendarDropDown);
-        String[] items = new String[]{"Month", "Week", "Day", "Calendar View"};
-
-        //create an adapter to describe how the items are displayed,
-        //adapters are used in several places in android.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
-
-        buttonAddEvent = (Button) findViewById(R.id.buttonAddEvent);
-        buttonAddEvent.setOnClickListener(new View.OnClickListener() {
-
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // on click opens data input view for new event
-
-                showAlertDialogButtonClicked(view);
-
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mymenu, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.item1:
-                Intent newInt = new Intent(this, myEventActivity.class);
-                startActivity(newInt);
-                return true;
-            case R.id.item2:
-                Intent newInt2 = new Intent(this, settingsActivity.class);
-                startActivity(newInt2);
-                return true;
-            case R.id.item3:
-                Toast.makeText(this, "About was selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item4:
-                Toast.makeText(this, "Already in event creation screen", Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
-
-    public void showAlertDialogButtonClicked(View view) {
-
-        // setup the alert builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Add new task");
-
-        // add a list
-        String[] options = {"Manually", "Scan QR code"};
-        builder.setItems(options, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0: // manually
-                        Intent intent = new Intent(getBaseContext(), addNewEvent.class);
-                        startActivity(intent);
-                    case 1: // qr code
-                }
-            }
-        });
-
-        // create and show the alert dialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
 }
